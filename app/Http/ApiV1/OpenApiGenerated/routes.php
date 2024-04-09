9a05d13af2c4939ec6d4e1f1e451b6db9adec808
Psychosoc1al <tohichpetrov@gmail.com>
2024-04-09 04:50:34 +0300

    Change API docs

--- app/Http/ApiV1/OpenApiGenerated/routes.php
+++ app/Http/ApiV1/OpenApiGenerated/routes.php
@@ -5,7 +5,9 @@
  * Do NOT edit it manually. Run `php artisan openapi:generate-server`.
  */
 
-use App\Http\ApiV1\Modules\Foos\Controllers\FoosController;
+use App\Http\ApiV1\Modules\Reservations\Controllers\ReservationsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('examples/{id}', [FoosController::class, 'get'])->name('getExample');
+Route::get('tables/status', [ReservationsController::class, 'getStatus']);
+Route::post('reservations', [ReservationsController::class, 'createReservation']);
+Route::delete('reservations/{id}', [ReservationsController::class, 'deleteReservation']);

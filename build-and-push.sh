093fa8fad931e68013a7154f44a4ea9c85232249
Psychosoc1al <tohichpetrov@gmail.com>
2024-06-06 10:54:18 +0300

    Update builder script

--- build-and-push.sh
+++ build-and-push.sh
@@ -2,5 +2,5 @@
 
 #sudo docker login -u psychosoc1al   # only once
 sudo docker-compose build
-sudo docker tag reservations psychosoc1al/reservations
+sudo docker tag reservations_app psychosoc1al/reservations
 sudo docker push psychosoc1al/reservations
